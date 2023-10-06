801cbcfec9f32e3fea7926010ed29626442ea229
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2023-10-06 21:21:11 +0530

    Nala-main removed

--- Nala Ubuntu/nala-main/debian/nala.fish
+++
@@ -1,1 +0,0 @@
-complete --command nala --no-files --arguments "(env _NALA_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=get-args _TYPER_COMPLETE_ARGS=(commandline -cp) nala)" --condition "env _NALA_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=is-args _TYPER_COMPLETE_ARGS=(commandline -cp) nala"
