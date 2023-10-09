655c3b315349033352e8444b5946752ebabb5f74
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2023-10-09 11:18:11 +0530

    Create Nala Installer.sh
    Separated the Nala installation file

---
+++ Nala Ubuntu/Nala Installer.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Nala for all
+echo "Nala is a front-end for libapt-pkg. Specifically we interface using the python-apt api."
+echo
+echo "Starting nala installation"
+sudo apt install nala
+echo
+echo "Installation Complete"
+echo "Verifying installation..."
+sudo nala --version
+echo "Verification complete"
+echo
+# Final Check
+echo "Save and close other apps that you are using now, we will check for updates, fix installations"
+echo
+echo "We will start updating in 30 seconds..."
+sleep 30
+sudo apt update
+sudo apt-file update
+sudo apt list --upgradable
+sudo apt upgrade -y
+echo
+echo "Fixing Installations..."
+sudo apt --fix-broken install -y
+echo
+echo "Rebooting Ubuntu in 1 min..."
+sleep 60
+exit 1
