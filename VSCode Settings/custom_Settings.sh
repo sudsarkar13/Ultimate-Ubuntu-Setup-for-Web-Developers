5e93cd6bf65a29d12cd25730b6eb25a134c586ca
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-04-13 20:14:59 +0530

    Update

--- VSCode Settings/custom_Settings.sh
+++ VSCode Settings/custom_Settings.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-# Define the desired format
-format="\u@\h:\w"
+# Define the lines to add to the .bashrc file
+lines_to_add="# Truncate the path in the prompt
+PS1='\${debian_chroot:+(\$debian_chroot)}\\\u@\\\h:\${BLUE} \\\W\${RESET_COLOR}\\\$ '
 
-# Append the command to trim the bash path to $PWD with the desired format to ~/.bashrc
-echo "export PS1=\"\$(echo -n \$format)\"" >> ~/.bashrc
+# Define color codes
+RESET_COLOR='\\\[\033[0m\\\]'
+GREEN='\\\[\033[0;32m\\\]'
+BLUE='\\\[\033[0;34m\\\]'
 
-# Source the updated ~/.bashrc to apply the changes immediately
-source ~/.bashrc
+# Check if the lines are already in .bashrc
+if grep -Fxq "$lines_to_add" ~/.bashrc; then
+    echo "Lines already exist in .bashrc. No changes made."
+else
+    # Add the lines to .bashrc
+    echo "$lines_to_add" >> ~/.bashrc
+    echo "Lines added to .bashrc."
+fi
