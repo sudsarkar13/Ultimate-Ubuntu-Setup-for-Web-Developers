64b5ccb697778be1e2bd2ed15201cccd1390a047
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2025-04-26 15:52:48 +0530

    feat ✨: add logging and user confirmation for theme installation
    - The main goal of the changes is to add logging功能 and modify the automated MacOS theme installation script to request user confirmation before proceeding with the installation, handling two possible user responses, yes or no.

--- Mac OS Theme/Downloader.sh
+++ Mac OS Theme/Downloader.sh
@@ -13,8 +13,6 @@ exec 2> >(tee $LOG_FILE >&2)
 # Install macOs themes for Ubuntu
 # Usage: ./Downloader.sh
 
-
-
 # Download the gtk themes inside Downloads folder
 mkdir -p ~/MacOS-Theme/
 cd ~/MacOS-Theme/
@@ -25,7 +23,6 @@ echo
 git clone https://github.com/vinceliuice/WhiteSur-icon-theme.git
 echo "Downloaded WhiteSur-icon-theme"
 echo
-
 # Download wallpapers for MacOs themes
 git clone https://github.com/vinceliuice/WhiteSur-wallpapers.git
 echo "Downloaded WhiteSur-wallpapers"
@@ -41,16 +38,67 @@ echo "Please wait while the installation script is being created..."
 cat << EOF > ~/MacOS-Theme/installer.sh
 #!/bin/bash
 
+# File to store logs
+LOG_FILE="logs.txt"
+
+# Remove the existing log file if it exists
+rm -f $LOG_FILE
+
+# Redirect stdout and stderr to a new log file
+exec > >(tee $LOG_FILE)
+exec 2> >(tee $LOG_FILE >&2)
+echo "Log file created at $LOG_FILE"
+
 # Usage: ./installer.sh
 echo "Installing required packages and extension manager..."
 sudo apt-get install -y gnome-tweaks gnome-shell-extension-manager
 echo
 sudo ubuntu-drivers autoinstall
 echo
-sleep 10
-exit
-
+sleep 5
+echo "Would you like to install the macOS theme on your system now?"
+read -p "Please enter [y/n]: " response
+if [[ "$response" == "y" ]]; then
+    echo "Proceeding with the installation..."
+    echo
+    echo "Starting the installation of the theme..."
+    sleep 5
+    echo
+    echo "Switching to WhiteSur-gtk-theme directory..."
+    cd ~/MacOS-Theme/WhiteSur-gtk-theme
+    echo
+    echo "Installing the theme..."
+    sleep 2
+    echo
+    echo "Installing the theme into gtk4.0 config for libadwaita..."
+    ./install.sh -o normal -a normal -t blue -s standard -m -N glassy -l -f -HD --round --shell -i apple -sf
+    echo
+    echo "Theme installation for gtk4.0 config for libadwaita is complete"
+    echo
+    echo "Installing theme tweaks..."
+    sleep 2
+    echo
+    echo "Applying gdm theme tweaks..."
+    echo
+    echo "Running ./tweaks.sh ..."
+    sleep 5
+    sudo ./tweaks.sh -g
+    ./tweaks.sh -c dark -t blue -s standard -d
+    echo
+    echo "Applying tweaks in the root..."
+    sudo ./tweaks.sh -c dark -t blue -s standard -d
+    echo
+    echo "Tweaks applied successfully"
+else
+    echo
+    echo "Installation aborted."
+    sleep 20
+    exit 1
+fi
 EOF
+echo
+# Make the installer script executable
+echo "Making the installer script executable..."
 chmod +x ~/MacOS-Theme/installer.sh
 echo
 echo "Installation script created"
@@ -75,8 +123,7 @@ cat << EOF > ~/MacOS-Theme/Instructions.txt
 2. Blur my Shell by aunetx
 3. Apps Menu by fmuellner
 4. Compiz alike magic lamp effect by hermes83
-
-For this effect to work go to terminal and type "gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'"
+    For this effect to work go to terminal and type "gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'"
 
 5. Emoji Copy by FelipeFTN 
 6. Clipboard Indicator by Tudmotu
@@ -87,5 +134,5 @@ For this effect to work go to terminal and type "gsettings set org.gnome.shell.e
 EOF
 echo
 echo "Instructions file generated Successfully"
-sleep 60
+sleep 30
 exit
